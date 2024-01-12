24ab1902ad39344c8317248b66d2d8fd104163f7
SonaliAkshay <154811163+SonaliAkshay@users.noreply.github.com>
2024-01-12 16:10:38 +0530

    sonaksh project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../src/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar></Topbar>
     </div>
   );
 }
 
+
+function Topbar()
+{
+  return(
+    <div className="row align-items-center topSec bgOrange">
+      <div class="col-md-8">
+          <div className="topbar-left-area">
+              <p>Contact Us : 7741018344</p>
+           </div>
+      </div>
+      <div className='col-md-4'>
+            <div class="topbar-social-icons">
+               
+            </div>
+      </div>
+    </div>
+    
+  )
+}
 export default App;
